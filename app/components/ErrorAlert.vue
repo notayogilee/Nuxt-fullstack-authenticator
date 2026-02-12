134f10d7e3c9949afb96e3931c19644919cc1b05
Lee Castelani <notayogilee@gmail.com>
2026-02-12 17:52:51 -0500

    Add client side error handling on registration page.

--- app/components/ErrorAlert.vue
+++ app/components/ErrorAlert.vue
@@ -22,24 +22,4 @@ const props = defineProps({
 .shake-enter-active {
   animation: shake 0.5s;
 }
-
-@keyframes shake {
-  0%,
-  100% {
-    transform: translateX(0);
-  }
-  10%,
-  30%,
-  50%,
-  70%,
-  90% {
-    transform: translateX(-10px);
-  }
-  20%,
-  40%,
-  60%,
-  80% {
-    transform: translateX(10px);
-  }
-}
 </style>
